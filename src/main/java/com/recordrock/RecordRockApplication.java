b3cac61a7eea324ecdec374ffc2b6b81cce725a7
blackmount22 <moongang1022@naver.com>
2023-11-07 11:34:05 +0900

    기본 세팅

---
+++ src/main/java/com/recordrock/RecordRockApplication.java
@@ -0,0 +1,13 @@
+package com.recordrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordRockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordRockApplication.class, args);
+	}
+
+}
